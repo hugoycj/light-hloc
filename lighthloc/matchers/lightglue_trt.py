Fix normalization

lighthloc/matchers/lightglue_trt.py
@@ -2,7 +2,7 @@ from ..utils.base_model import BaseModel
 import onnxruntime as ort
 import torch
 import time
-
+import numpy as np
 class LightGlue(BaseModel):
     default_conf = {
         'features': 'superpoint',
@@ -33,10 +33,25 @@ class LightGlue(BaseModel):
         self.net = ort.InferenceSession(
             'lighthloc/matchers/superpoint_lightglue.onnx', sess_options=sess_options, providers=providers
         )
-        
+    
+    @staticmethod
+    def normalize_keypoints(kpts: np.ndarray, h: int, w: int) -> np.ndarray:
+        size = np.array([w, h])
+        shift = size / 2
+        scale = size.max() / 2
+        kpts = (kpts - shift) / scale
+        return kpts.astype(np.float32)
+
     def _forward(self, data):
         kpts0, kpts1 = data["keypoints0"].cpu().numpy(), data["keypoints1"].cpu().numpy()
-                
+        
+        kpts0 = self.normalize_keypoints(
+                        kpts0, data['image0'].shape[2], data['image0'].shape[3]
+                    )
+        kpts1 = self.normalize_keypoints(
+                        kpts1, data['image1'].shape[2], data['image1'].shape[3]
+                    )
+        
         desc0 = data['descriptors0'].transpose(-1, -2).cpu().numpy()
         desc1 = data['descriptors1'].transpose(-1, -2).cpu().numpy()
 