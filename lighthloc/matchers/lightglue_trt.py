c2e00332a82620540a4b111947094079b901c022
hugoycj <576550489@qq.com>
2023-10-31 15:45:02 +0800

    Add lightglue_trt

---
+++ lighthloc/matchers/lightglue_trt.py
@@ -0,0 +1,61 @@
+from ..utils.base_model import BaseModel
+from .modules.lightglue_onnx import LightGlue as LightGlue_
+import onnxruntime as ort
+import torch
+import time
+
+class LightGlue(BaseModel):
+    default_conf = {
+        'features': 'superpoint',
+        'depth_confidence': 0.95,
+        'width_confidence': 0.99,
+    }
+    required_inputs = [
+        'image0', 'keypoints0', 'descriptors0',
+        'image1', 'keypoints1', 'descriptors1',
+    ]
+
+    def _init(self, conf):
+        sess_options = ort.SessionOptions()
+        providers=["CUDAExecutionProvider", "CPUExecutionProvider"]
+        trt = True
+        if trt:
+            providers = [
+                (
+                    "TensorrtExecutionProvider",
+                    {
+                        "trt_fp16_enable": True,
+                        "trt_engine_cache_enable": True,
+                        "trt_engine_cache_path": "weights/cache",
+                    },
+                )
+            ] + providers
+            
+        self.net = ort.InferenceSession(
+            'lighthloc/matchers/modules/weights/superpoint_lightglue.onnx', sess_options=sess_options, providers=providers
+        )
+        
+    def _forward(self, data):
+        kpts0, kpts1 = data["keypoints0"].cpu().numpy(), data["keypoints1"].cpu().numpy()
+                
+        desc0 = data['descriptors0'].transpose(-1, -2).cpu().numpy()
+        desc1 = data['descriptors1'].transpose(-1, -2).cpu().numpy()
+
+        m0, m1, mscores0, mscores1 = self.net.run(
+                None,
+                {
+                    "kpts0": kpts0,
+                    "kpts1": kpts1,
+                    "desc0": desc0,
+                    "desc1": desc1,
+                },
+            )
+        end_time = time.time()
+
+
+        return {
+            "matches0": torch.from_numpy(m0),
+            "matches1": torch.from_numpy(m1),
+            "matching_scores0": torch.from_numpy(mscores0),
+            "matching_scores1": torch.from_numpy(mscores1),
+        }
